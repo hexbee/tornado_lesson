1242afd6aa9fe56728619c0f37bef19bd939bfe6
hexbee <hexbee@aliyun.com>
2017-10-20 22:32:54 +0800

    add module "AdModule"

--- utils/uimodules.py
+++ utils/uimodules.py
@@ -4,3 +4,18 @@ from tornado.web import UIModule
 class UiModule(UIModule):
     def render(self, *args, **kwargs):
         return 'Im ui_modules'
+
+
+class AdModule(UIModule):
+    def render(self, *args, **kwargs):
+        return self.render_string('07ad.html')
+
+    def css_files(self):
+        return '/static/css/King_Chance_Layer7.css'
+
+    def javascript_files(self):
+        return [
+            "/static/js/jquery_1_7.js",
+            "/static/js/King_Chance_Layer.js",
+            "/static/js/King_layer_test.js",
+        ]
